f7b7b737940761b270eba25437cb3a029bf5fab5
Anna Fomina <ann.fomi77@gmail.com>
2025-09-02 11:41:13 +0300

    1 tund

---
+++ ShopTARge24/ShopTARge24.Data/ShopTARge24Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ShopTARge24.Core.Domain;
+
+namespace ShopTARge24.Data
+{
+    public class ShopTARge24Context : DbContext
+    {
+        public DbSet<Spaceships> Spaceships { get; set; }
+    }
+}
--- ShopTARge24/ShopTARge24/Controllers/SpaceshipsController.cs
+++ ShopTARge24/ShopTARge24/Controllers/SpaceshipsController.cs
@@ -1,11 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARge24.Data;
 
 namespace ShopTARge24.Controllers
 {
+
     public class SpaceshipsController : Controller
     {
+        private readonly ShopTARge24Context _context;
+
+        public SpaceshipsController
+            (
+            
+            )
+        {
+
+        }
         public IActionResult Index()
         {
+            var result = 
             return View();
         }
     }
