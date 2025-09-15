2a5be9be54173d6ca67dba5ce2b6b3145d6e06bb
Anna Fomina <ann.fomi77@gmail.com>
2025-09-15 17:42:35 +0300

    Add Delete Spaceship

--- ShopTARge24/ShopTARge24/Controllers/SpaceshipsController.cs
+++ ShopTARge24/ShopTARge24/Controllers/SpaceshipsController.cs
@@ -91,5 +91,17 @@ namespace ShopTARge24.Controllers
 
             return View(vm);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmation(Guid id)
+        {
+            var spaceship = await _spaceshipServices.Delete(id);
+
+            if (spaceship == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
