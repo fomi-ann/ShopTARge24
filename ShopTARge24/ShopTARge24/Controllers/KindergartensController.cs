6092515cff4953384ea93e5a385970c27a89a882
Anna Fomina <ann.fomi77@gmail.com>
2025-09-20 21:20:27 +0300

    Add Kindergarten btn to nav menu, KinergartensController

---
+++ ShopTARge24/ShopTARge24/Controllers/KindergartensController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARge24.Data;
+using ShopTARge24.Models.Kindergartens;
+
+namespace ShopTARge24.Controllers
+{
+    public class KindergartensController : Controller
+    {
+        private readonly ShopTARge24Context _context;
+
+        public KindergartensController
+            (
+            ShopTARge24Context context
+            )
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = _context.Kindersgartens
+                .Select(x => new KindergartenIndexViewModel
+                {
+                    Id = x.Id,
+                    KindergartenName = x.KindergartenName,
+                    GroupName = x.GroupName,
+                    CreatedAt = x.CreatedAt,
+                    TeacherName = x.TeacherName,
+                    ChildrenCount = x.ChildrenCount,
+                });
+            return View(result);
+        }
+    }
+}
+
+//public class KindergartenIndexViewModel
+//{
+//    public Guid Id { get; set; }
+//    public string GroupName { get; set; }
+//    public int ChildrenCount { get; set; }
+//    public string KindergartenName { get; set; }
+//    public string TeacherName { get; set; }
+//    public DateTime CreatedAt { get; set; }
+//    public DateTime UpdatedAt { get; set; }
+
+//}
