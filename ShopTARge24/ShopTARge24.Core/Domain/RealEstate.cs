bf8a8a1581c0f86e68e7c1fa0d75b94635adaace
Anna Fomina <ann.fomi77@gmail.com>
2025-09-29 19:44:56 +0300

    Update RealEstate.cs

--- ShopTARge24/ShopTARge24.Core/Domain/RealEstate.cs
+++ ShopTARge24/ShopTARge24.Core/Domain/RealEstate.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace ShopTARge24.Core.Domain
 {
-    internal class RealEstate
+    public class RealEstate
     {
+        public Guid? Id { get; set; }
+        public double? Area { get; set; }
+        public string? Location { get; set; }
+        public int? RoomNumber { get; set; }
+        public string? BuildingType { get; set;}
+
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
     }
 }
