42cd2bf660c74218f4ff5719edfc389c2e72a5aa
Anna Fomina <ann.fomi77@gmail.com>
2025-10-06 10:55:14 +0300

    RealEstate CreateUpdateView

--- ShopTARge24/ShopTARge24/Models/RealEstate/RealEstateImageViewModel.cs
+++ ShopTARge24/ShopTARge24/Models/RealEstate/RealEstateImageViewModel.cs
@@ -3,6 +3,8 @@
     public class RealEstateImageViewModel
     {
         public Guid Id { get; set; }
+
+        public string? Image {  get; set; }
         public string? ImageTitle { get; set; }
         public byte[]? ImageData { get; set; }
         public Guid? RealEstateId { get; set; }
