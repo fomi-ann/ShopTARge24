67afc607e181c845436f7060d259d5093b7b9390
Anna Fomina <ann.fomi77@gmail.com>
2025-10-13 10:45:12 +0300

    WeatherForecast Api Progress

---
+++ ShopTARge24/ShopTARge24.ApplicationServices/Services/WeatherForecastServices.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using ShopTARge24.Core.Dto;
+using ShopTARge24.Core.ServiceInterface;
+
+namespace ShopTARge24.ApplicationServices.Services
+{
+    public class WeatherForecastServices : IWeatherForecastServices
+    {
+        public async Task<AccuLocationWeatherResultDto> AccuWeatherResult(AccuLocationWeatherResultDto dto)
+        {
+            var response = $"https://api.weatherapi.com/v1/current.json";
+
+            using (var client = new HttpClient())
+            {
+                var httpResponse = await client.GetAsync(response);
+                string json = await httpResponse.Content.ReadAsStringAsync();
+            
+                List<AccuLocationRootDto> weatherData =
+                    JsonSerializer.Deserialize<List<AccuLocationRootDto>>(json);
+            }
+            return dto;
+        }
+    }
+}
---
+++ ShopTARge24/ShopTARge24.Core/Dto/AccuLocationRootDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopTARge24.Core.Dto
+{
+    public class AccuLocationRootDto
+    {
+        public class Root
+        {
+            public string? LocalObservationDateTime { get; set; }
+            public int? EpochTime { get; set; }
+            public string? WeatherText { get; set; }
+            public int? WeatherIcon { get; set; }
+            public bool? HasPrecipitation { get; set; }
+            public string? PrecipitationType { get; set; }
+            public bool? IsDayTime { get; set; }
+            public string? MobileLink { get; set; }
+            public string? Link { get; set; }
+            public AccuTemperatureDto? Temperature { get; set; }
+        }
+        public class AccuTemperatureDto
+        {
+            public AccuWeatherUnitDto? Metric { get; set; }
+            public AccuWeatherUnitDto? Imperial { get; set; }
+        }
+
+        public class AccuWeatherUnitDto
+        {
+            public int? UnitType { get; set; }
+            public double? Value { get; set; }
+            public string? Unit { get; set; }
+        }
+
+    }
+}
