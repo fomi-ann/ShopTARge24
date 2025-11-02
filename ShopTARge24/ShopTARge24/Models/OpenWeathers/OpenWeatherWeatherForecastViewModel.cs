ea191a63cf75084878979a9f363edcd8a1992ebe
Anna Fomina <ann.fomi77@gmail.com>
2025-11-02 19:30:56 +0200

    Update dto,View and ViewModel for OpenWeather response,

--- ShopTARge24/ShopTARge24/Models/OpenWeathers/OpenWeatherWeatherForecastViewModel.cs
+++ ShopTARge24/ShopTARge24/Models/OpenWeathers/OpenWeatherWeatherForecastViewModel.cs
@@ -2,5 +2,15 @@
 {
     public class OpenWeatherWeatherForecastViewModel
     {
+        //City Name
+        public string Name { get; set; }
+        public double Temp {  get; set; }
+        public double FeelsLike { get; set; }
+        public int Humidity { get; set; }
+        public int Pressure { get; set; }
+        //Wind Speed
+        public double Speed { get; set; }
+        //Weather Conditions
+        public string Desctiption { get; set; }
     }
 }
