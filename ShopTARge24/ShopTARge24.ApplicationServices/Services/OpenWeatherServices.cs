05c40e44a223cd17ce98f970147d5abdef7594f1
Anna Fomina <ann.fomi77@gmail.com>
2025-11-02 22:35:40 +0200

    Update OpenWeatherServices.cs

--- ShopTARge24/ShopTARge24.ApplicationServices/Services/OpenWeatherServices.cs
+++ ShopTARge24/ShopTARge24.ApplicationServices/Services/OpenWeatherServices.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ShopTARge24.ApplicationServices.Services
@@ -13,18 +14,33 @@ namespace ShopTARge24.ApplicationServices.Services
 
     {
         private readonly IConfiguration _configuration;
+        private readonly HttpClient _httpClient;
 
-        public OpenWeatherServices(IConfiguration configuration)
+        public OpenWeatherServices
+            (
+            IConfiguration configuration, 
+            HttpClient httpClient
+            )
         {
             _configuration = configuration;
-            //OpenWeatherApiKey
+            _httpClient = httpClient;
         }
 
         public async Task<OpenWeathersDto> OpenWeatherResult(OpenWeathersDto dto)
         {
             string apiKey = _configuration["OpenWeatherApiKey"];
+            
             var url = $"https://api.openweathermap.org/data/2.5/weather?q={dto.Name}&appid={apiKey}&units=metric";
-            return null;
+            
+            var response = await _httpClient.GetAsync( url );
+            response.EnsureSuccessStatusCode();
+
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+
+
+            var weather = JsonSerializer.Deserialize<OpenWeathersDto>(jsonResponse);
+            
+            return weather;
         }
     }
 }
