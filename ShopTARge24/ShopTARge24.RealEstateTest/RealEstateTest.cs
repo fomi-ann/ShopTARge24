a43fe461fb27b47d3001dcd3fcbdc16132495bae
Anna Fomina <ann.fomi77@gmail.com>
2025-11-03 10:14:38 +0300

    UnitTest for RealEstate

---
+++ ShopTARge24/ShopTARge24.RealEstateTest/RealEstateTest.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopTARge24.RealEstateTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ ShopTARge24/ShopTARge24.RealEstateTest/TestBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using ShopTARge24.RealEstateTest.Macros;
+using ShopTARge24.ApplicationServices.Services;
+using ShopTARge24.Core.ServiceInterface;
+using ShopTARge24.Data;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace ShopTARge24.RealEstateTest
+{
+    public abstract class TestBase
+    {
+    protected IServiceProvider ServiceProvider { get; set; }
+        protected TestBase()
+        {
+            var services = new ServiceCollection();
+            SetupServices(services);
+            ServiceProvider = services.BuildServiceProvider();
+        }
+
+        public virtual void SetupServices(IServiceCollection services)
+        {
+            services.AddScoped<IRealEstateServices, RealEstateServices>();
+
+            services.AddDbContext<ShopTARge24Context>(x =>
+            {
+                x.UseInMemoryDatabase("TEST");
+                x.ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+            });
+
+            RegisterMacros(services);
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        private void RegisterMacros(IServiceCollection services)
+        {
+            var macroBaseType = typeof(IMacros);
+
+            var macros = macroBaseType.Assembly.GetTypes()
+                .Where(t => macroBaseType.IsAssignableFrom(t)
+                && !t.IsInterface && !t.IsAbstract);
+
+            foreach (var macro in macros)
+            {
+                services.AddSingleton(macro);
+            }
+        }
+    }
+}
