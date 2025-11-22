e7d8e1875c4169ad96867d2d8223b3bc910c2fdd
Anna Fomina <ann.fomi77@gmail.com>
2025-11-22 15:10:00 +0300

    xUnit test project setup

---
+++ ShopTARge24/ShopTARge24.KindergartenTest/TestBase.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ShopTARge24.ApplicationServices.Services;
+using ShopTARge24.Core.ServiceInterface;
+using ShopTARge24.Data;
+using ShopTARge24.KindergartenTest.Macros;
+
+namespace ShopTARge24.KindergartenTest
+{
+    public abstract class TestBase
+    {
+        protected IServiceProvider ServiceProvider { get; set; }
+
+        protected TestBase() {
+            var services = new ServiceCollection();
+            SetupServices(services);
+            ServiceProvider = services.BuildServiceProvider();
+        }
+
+        public virtual void SetupServices(IServiceCollection services)
+        {
+            services.AddScoped<IKindergartenServices, KindergartenServices>();
+            services.AddScoped<IFileServices, IFileServices>();
+            services.AddScoped<IHostEnvironment,  IHostEnvironment>();
+
+            services.AddDbContext<ShopTARge24Context>(x =>
+            {
+                x.UseInMemoryDatabase("TEST");
+                x.ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+            });
+
+            RegisterMacros(services);
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public T Svc<T>()
+        {
+            // Resolve service from the service provider
+            return ServiceProvider.GetService<T>();
+        }
+
+        private void RegisterMacros(IServiceCollection services)
+        {
+            var macroBaseType = typeof(IMacros);
+
+            var macros = macroBaseType.Assembly.GetTypes()
+                .Where(t => macroBaseType.IsAssignableFrom(t)
+                && !t.IsInterface && !t.IsAbstract);
+
+            foreach (var macro in macros)
+            {
+                services.AddSingleton(macro);
+            }
+        }
+    }
+}
