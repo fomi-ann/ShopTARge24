77d95107a7ecee93626ac3655752da4e34be3e83
Anna Fomina <ann.fomi77@gmail.com>
2025-11-25 11:25:04 +0300

    EmailDto & EmailServices

---
+++ ShopTARge24/ShopTARge24.ApplicationServices/Services/EmailServices.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using MimeKit;
+using ShopTARge24.Core.Dto;
+
+namespace ShopTARge24.ApplicationServices.Services
+{
+    public class EmailServices
+    {
+        private readonly IConfiguration _config;
+
+        public EmailServices
+            (
+                IConfiguration config
+            )
+        {
+            _config = config;
+        }
+        public void SendEmail(EmailDto dto)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_config.GetSection("EmailUserNmae").Value));
+            email.To.Add(MailboxAddress.Parse(dto.To));
+            email.Subject = dto.Subject;
+
+            var builder = new BodyBuilder
+            {
+                HtmlBody = dto.Body
+            };
+
+            //failide lisamine
+            // kontrollib faili suurust ja siis saadab teele
+
+        }
+    }
+}
+
